38f5be2695fe3b00e5e5cd0bfe9004d9aa9656ea
RMo-Sloth <robmonhemius@gmail.com>
2018-08-07 23:46:19 +0200

    Added unread mail interface to the dashboard.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  hasUnreadMails: boolean = false; // TODO: should be a property of an account service
   constructor() { }
 
   ngOnInit() {
