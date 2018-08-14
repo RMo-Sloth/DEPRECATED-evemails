97e36250beb1a3756066af80ab2dc0a712bf4054
RMo-Sloth <robmonhemius@gmail.com>
2018-08-14 18:56:01 +0200

    re-enabled old email service as singleton.

--- src/app/received-mails/received-mails.component.ts
+++ src/app/received-mails/received-mails.component.ts
@@ -14,18 +14,17 @@ import{ MailService } from '../services/user-account/mail.service';
   styleUrls: ['./received-mails.component.css']
 })
 export class ReceivedMailsComponent implements OnInit {
-  mailService: MailService;
   account: Character;
   mails: Mail[];
   navigationButtons; // TODO: typecheck
   constructor(
     public  appStateService : AppStateService,
+    public mailService: MailService,
     private route: ActivatedRoute,
     public userAccountService: UserAccountService
   ) {
     const account_id = parseInt( this.route.snapshot.paramMap.get('account_id') );
     this.account = this.userAccountService.get_account( account_id );
-    // this.mailService = this.userAccountService.get_mailService();
     this.navigationButtons = [
       { faClass: 'home', routerUrl: '/dashboard'},
       { faClass: 'search', routerUrl: '/dashboard'},
@@ -34,8 +33,8 @@ export class ReceivedMailsComponent implements OnInit {
   }
   ngOnInit() {
     this.appStateService.currentPageName = this.account.name;
-    // this.mailService.getMails()
-    //     .subscribe(mails => this.mails = mails);
+    this.mailService.getMails()
+        .subscribe(mails => this.mails = mails);
   }
   // TODO: click on a read-more button to retreive more mails
   // TODO: apply filters on ngOnInit() ( before view )
