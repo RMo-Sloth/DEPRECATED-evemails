f43c0bb06a3b898a9c7c170884bd83bc1483d7c1
RMo-Sloth <robmonhemius@gmail.com>
2018-08-14 18:44:22 +0200

    refactored userAccountService to account

--- src/app/mail/mail.component.ts
+++ src/app/mail/mail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Mail } from '../classes/mail';
+import{ Character } from '../classes/Character';
 
 import { AppStateService } from '../app-state.service';
 import{ UserAccountService } from '../services/user-account.service';
@@ -14,18 +15,17 @@ import{ MailService } from '../services/user-account/mail.service';
   styleUrls: ['./mail.component.css']
 })
 export class MailComponent implements OnInit {
-  userAccountService: UserAccountService;
-  mailService: MailService;
-  account_id: number;
+  account: Character;
   mail: Mail;
   navigationButtons; // TODO: typecheck
   constructor(
     public appStateService: AppStateService,
+    public userAccountService: UserAccountService,
+    public mailService: MailService,
     private route: ActivatedRoute
   ) {
     const account_id = parseInt( this.route.snapshot.paramMap.get('account_id') );
-    this.userAccountService = this.appStateService.get_account( account_id );
-    this.mailService = this.userAccountService.get_mailService();
+    this.account = this.userAccountService.get_account( account_id );
 
     this.navigationButtons = [
       { faClass: 'home', routerUrl: '/dashboard'},
@@ -37,7 +37,7 @@ export class MailComponent implements OnInit {
     let mailIndex = parseInt( this.route.snapshot.paramMap.get('mail_id') );
     this.mail = this.mailService.getMail( mailIndex );
         // .subscribe(mail => this.mail = mail);
-    this.appStateService.currentPageName = this.userAccountService.characterName;
+    this.appStateService.currentPageName = this.account.name;
   }
 
 }
