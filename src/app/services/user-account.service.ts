aec1e739efea1f1dd4aed9675084a22d13f1e986
RMo-Sloth <robmonhemius@gmail.com>
2018-08-16 00:03:20 +0200

    checked vs duplicate accounts

--- src/app/services/user-account.service.ts
+++ src/app/services/user-account.service.ts
@@ -19,50 +19,44 @@ export class UserAccountService {
       let accounts = JSON.parse( localStorage.getItem('accounts') );
       accounts.forEach( account => {
         this.add_account( account.characterId, account.characterName );
-
-        // TODO: refactor to mailService
-        // this.add_account(
-          // new UserAccountService(
-          //   'accessToken',
-          //   'refreshToken',
-          //   'tokenExpirationTime'
-          // )
-        // );
       });
     }
     this.accounts.forEach( account => {
       this.characterService.append_characterPortraits( account );
     });
-    // TODO: httpService should contain the
-    // * accessToken don't need!
-    // * refreshToken
-    // * tokenExpirationTime
-    // TODO: all the below:
-    // after reirect of teh account verification we will get a:
-    // * access token
-    // * refresh token
-    // * token expiration time ( will need to check this every minute through the app-state? And refresh when necessary. The appstate will set the frequency and UserAccountService will figure out when to update )
-    // * With this info we can verify through our own server and get the characterId
-    // * characterId
-    // characterId, and refresh token should be stored in a cookie
-    // With the token and characterId we can gather all kinds of information
-    // * characterImageUrls
-    // * characterName
   }
-  // modify this.account
   private add_account( characterId, characterName ):void{
-    // TODO: prevent creation of identical characters
-    let character = new Character( characterId );
-    character.name = characterName;
-    this.accounts.push( character );
+    let exists = this.accounts.some( account => {
+      return account.characterId === characterId;
+    });
+    if( exists === false ){
+      let character = new Character( characterId );
+      character.name = characterName;
+      this.accounts.push( character );
+    }
   }
   public get_account( characterIndex: number ): Character{
     for( let i=0; this.accounts.length > i; i++ ){
       if( this.accounts[i].characterId  === characterIndex ){
         return this.accounts[i];
       }
-      // TEMP: alert
     }
-    alert('Account can not be found!');
+    // TEMP: alert
+    console.log('Account can not be found! Maybe look somewhere else?!?');
   }
 }
+
+
+
+// TODO: write a validationService
+// TODO: all the below:
+// after reirect of teh account verification we will get a:
+// * access token
+// * refresh token
+// * token expiration time ( will need to check this every minute through the app-state? And refresh when necessary. The appstate will set the frequency and UserAccountService will figure out when to update )
+// * With this info we can verify through our own server and get the characterId
+// * characterId
+// characterId, and refresh token should be stored in a cookie
+// With the token and characterId we can gather all kinds of information
+// * characterImageUrls
+// * characterName
