6d1f9f0a715a3883b7ae90838d3d70c0c18e3adc
RMo-Sloth <robmonhemius@gmail.com>
2018-08-23 13:11:43 +0200

    Created empty methods.

--- src/app/services/verification/verification.service.ts
+++ src/app/services/verification/verification.service.ts
@@ -6,4 +6,37 @@ import { Injectable } from '@angular/core';
 export class VerificationService {
 
   constructor() { }
+
+  accounts: []; // TODO: stricter typecheck
+
+  public add_account(
+    characterIndex: number,
+    refreshToken: string,
+    accessToken: string ): void{
+
+  }
+  public remove_account( characterIndex: number ): void{
+
+  }
+
+  private get_refreshToken( characterIndex: number ): string{
+    // TEMP:
+    return 'refreshToken';
+  }
+  private update_refreshToken( characterIndex: number ): void{
+
+  }
+
+  public get_accessToken( characterIndex: number ): string{
+    // TEMP:
+    return 'accessToken';
+  }
+  private update_accessToken( characterIndex: number ): void{
+
+  }
+
+  public refresh_tokens( characterIndex: number ): void{
+    // TODO: recursive function with a timeOut based on the experationTime??
+  }
+
 }
