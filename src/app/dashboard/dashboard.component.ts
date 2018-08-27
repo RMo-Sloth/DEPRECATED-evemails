4844ad6f7b6b31c99398c1b421be83a3bc28f247
RMo-Sloth <robmonhemius@gmail.com>
2018-08-27 05:06:18 +0200

    Implemented get_accountInfo()

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
--- src/app/services/signup/signup.service.ts
+++ src/app/services/signup/signup.service.ts
@@ -1,25 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { UserAccountService } from '../user-account.service';
 import { LocalStorageService } from '../local-storage/local-storage.service';
 
+interface BasicAccountInfo{
+  CharacterID: number,
+  CharacterName: string,
+  CharacterOwnerHash: string,
+  ExpiresOn: string,
+  IntellectualProperty: string,
+  TokenType: string
+}
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
 
   constructor(
-    userAccountService: UserAccountService,
-    localStorageService: LocalStorageService
+    private http: HttpClient,
+    private userAccountService: UserAccountService,
+    private localStorageService: LocalStorageService
   ) { }
 
-  public signup_account( accessToken ){
-    
+  public signup_account( accessToken ): void{
   }
-  private get_accountInfo(){
-    // http-request using the accessToken
+  private get_accountInfo( accessToken ): Observable<BasicAccountInfo>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': `Bearer ${accessToken}`
+      })
+    };
+    // there was an issue where the old accountInfo got loaded repeatably.
+    // the accessToken is added to the url to prevent browser-caching.
+    return this.http.get<BasicAccountInfo>(`https://esi.tech.ccp.is/verify/?token=${accessToken}`, httpOptions);
   }
-  private register_account( characterId, accessToken, refreshToken ){
+  private register_account( characterIndex, accessToken, refreshToken ){
     this.userAccountService.add_account( characterIndex ); // TODO: should add accessToken too.
     this.localStorageService.add_account( characterIndex, accessToken, refreshToken ); // TODO: add expiration time of accessToken too
   }
