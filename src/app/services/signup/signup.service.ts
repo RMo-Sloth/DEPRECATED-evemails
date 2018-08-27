c9f2ccc5566946f08fcbd58dc5119fe6b3de9239
RMo-Sloth <robmonhemius@gmail.com>
2018-08-27 04:33:25 +0200

    implemented register_account

--- src/app/services/signup/signup.service.ts
+++ src/app/services/signup/signup.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 
+import { UserAccountService } from '../user-account.service';
+import { LocalStorageService } from '../local-storage/local-storage.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
 
-  constructor() { }
+  constructor(
+    userAccountService: UserAccountService,
+    localStorageService: LocalStorageService
+  ) { }
+
+  public signup_account( accessToken ){
+    
+  }
+  private get_accountInfo(){
+    // http-request using the accessToken
+  }
+  private register_account( characterId, accessToken, refreshToken ){
+    this.userAccountService.add_account( characterIndex ); // TODO: should add accessToken too.
+    this.localStorageService.add_account( characterIndex, accessToken, refreshToken ); // TODO: add expiration time of accessToken too
+  }
+
 }
