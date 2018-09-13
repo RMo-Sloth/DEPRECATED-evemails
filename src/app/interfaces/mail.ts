5b0a3aec72de6176fb630b37a00c86fb1bdb5787
RMo-Sloth <robmonhemius@gmail.com>
2018-09-13 20:01:10 +0200

    Added account property, corrected typo tiimestamp, removed comments

--- src/app/interfaces/mail.ts
+++ src/app/interfaces/mail.ts
@@ -1,14 +1,11 @@
 export interface Mail{
   readonly index: number;
+  readonly account: number;
   readonly labels: number[];
   readonly sender: number;
   readonly recipients: number[];
   readonly subject: string;
   readonly body?: string;
-  readonly tiimestamp: Date;
+  readonly timestamp: Date;
   readonly isRead: boolean;
 }
-
-// TODO: How to deal with undefined body?
-// * load the body to all mails seems overkill( but easier )
-// * retreive body with a get_body() function, that checks if the property exists. If not make the request. Where should the function be? Private function triggered by a get_mail() function?
