5bf883f6c92ba21dfb616b6778e6e89ef0953a62
RMo-Sloth <robmonhemius@gmail.com>
2018-09-13 19:53:05 +0200

    Created blank new-mail.service

---
+++ src/app/services/new-mail.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+import { NewMail } from '../interfaces/new-mail';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewMailService {
+
+  constructor() { }
+}
