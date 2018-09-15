8661dc8a514fc8096720ecab3a264f172a26c1d1
RMo-Sloth <robmonhemius@gmail.com>
2018-09-15 08:52:32 +0200

    Observable to generate Auth request with proper headers

--- src/app/services/account-http.service.ts
+++ src/app/services/account-http.service.ts
@@ -24,18 +24,20 @@ export class AccountHttpService {
 
   public get( url: string, account: number ):Observable<any>{
     // get the account
-    this.accountService.get_account( account )
-    .subscribe( account => {
-      this.setAuthorizationHeader( account.accessToken );
-      return this.http.get( url, this.httpHeaders );
-    }, error => {
-      console.error( error );
-    }); // end subscribe
+    return new Observable( observer => {
+      this.accountService.get_account( account )
+      .subscribe( account => {
+        this.setAuthorizationHeader( account.accessToken );
+        observer.next( this.http.get( url, this.httpHeaders ) );
+        observer.complete();
+      }, error => {
+        observer.error( error );
+      }); // end subscribe
+    }); // end observable
   }
   private setAuthorizationHeader( accessToken ): void {
-    this.httpOptions.headers.set('Authorization', `Bearer ${accessToken}`);
+    this.httpHeaders.headers.set('Authorization', `Bearer ${accessToken}`);
     console.log('need to remove this message after testing:');
     console.log( this.httpHeaders );
-
   }
 }
