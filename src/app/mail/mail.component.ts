93e4531e285fa817e824f462efd9579ca088e90b
RMo-Sloth <robmonhemius@gmail.com>
2018-09-22 17:25:31 +0200

    Refactored mailComponent

--- src/app/mail/mail.component.ts
+++ src/app/mail/mail.component.ts
@@ -1,44 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { Mail } from '../classes/mail/Mail';
-import{ Character } from '../classes/character/Character';
-import{ Account } from '../classes/account/Account';
+/* INTERFACES */
+import { Mail } from '../interfaces/mail';
+import { NavigationButton } from '../interfaces/navigation-button';
+// import { Mail } from '../classes/mail/Mail';
+// import{ Character } from '../classes/character/Character';
+// import{ Account } from '../classes/account/Account';
 
+/* SERVICES */
 import { PageTitleService } from '../services/page-title.service';
-import{ UserAccountService } from '../services/user-account.service';
-import{ MailService } from '../services/user-account/mail.service';
+import{ MailService } from '../services/mail.service';
 
 @Component({
   selector: 'app-mail',
   templateUrl: './mail.component.html',
   styleUrls: ['./mail.component.css']
 })
+
 export class MailComponent implements OnInit {
-  account: Account;
-  mail: Mail;
-  navigationButtons; // TODO: typecheck
+
+  private accountIndex: number;
+  private mailIndex: number;
+  private mail: Mail;
+  private navigationButtons: NavigationButton[];
+
   constructor(
-    public pageTitleService: PageTitleService,
-    public userAccountService: UserAccountService,
-    public mailService: MailService,
+    private pageTitleService: PageTitleService,
+    private mailService: MailService,
     private route: ActivatedRoute
   ) {
-    const account_id = parseInt( this.route.snapshot.paramMap.get('account_id') );
-    this.account = this.userAccountService.get_account( account_id );
-
+    this.accountIndex = parseInt( this.route.snapshot.paramMap.get('account_id') );
+    this.mailIndex = parseInt( this.route.snapshot.paramMap.get('mail_id') );
     this.navigationButtons = [
       { faClass: 'home', routerUrl: '/dashboard'},
-      { faClass: 'envelope', routerUrl: `/${account_id}/mails`}
+      { faClass: 'envelope', routerUrl: `/${this.accountIndex}/mails`}
     ];
   }
 
   ngOnInit() {
-    let mailIndex = parseInt( this.route.snapshot.paramMap.get('mail_id') );
-    this.mail = this.mailService.getMail( this.account, mailIndex );
-        // .subscribe(mail => this.mail = mail);
-    this.account.character.name$.asObservable().subscribe( name => {
-      this.pageTitleService.set_pageTitle( name );
+    this.mailService.get_mail( this.mailIndex, this.accountIndex )
+    .subscribe( mail => {
+      this.mail = mail;
+      console.log(mail)
     });
   }
 
