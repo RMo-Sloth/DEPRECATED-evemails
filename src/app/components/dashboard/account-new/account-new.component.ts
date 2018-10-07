0639f990eea31906194f815ed79ecad3159d4e04
RMo-Sloth <robmonhemius@gmail.com>
2018-10-07 23:32:01 +0200

    implemented production signup

--- src/app/components/dashboard/account-new/account-new.component.ts
+++ src/app/components/dashboard/account-new/account-new.component.ts
@@ -12,6 +12,10 @@ export class AccountNewComponent implements OnInit {
   ngOnInit() {
   }
   private account_signup(){
+      /* development signup */
       location.href="https://login.eveonline.com/oauth/authorize?response_type=token&redirect_uri=http://localhost:4200/dashboard&Client_id=ca211b71e15249ed8ce2d36f034f6024&scope=esi-mail.organize_mail.v1%20esi-mail.read_mail.v1%20esi-mail.send_mail.v1";
+      /* production signup
+      location.href="https://login.eveonline.com/oauth/authorize/?response_type=code&redirect_uri=https%3A%2F%2Fwww.eve-mails.com&client_id=31fb6d6b42ef4528a267376f4b73d19f&scope=esi-mail.organize_mail.v1%20esi-mail.read_mail.v1%20esi-mail.send_mail.v1"
+      */
   }
 }
