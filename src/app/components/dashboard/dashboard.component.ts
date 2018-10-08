a08ee5adef150cc87d846680bb50f2b752a1c3e0
RMo-Sloth <robmonhemius@gmail.com>
2018-10-08 03:00:10 +0200

    more general check for queryParameters.params.code

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -32,7 +32,7 @@ export class DashboardComponent implements OnInit {
 
     this.route.queryParamMap.subscribe( (queryParameters: any ) => {
       /* check if registration with an access_token is used */
-      if( queryParameters.params.code == true ) {
+      if( queryParameters.params.code ) {
         this.signupService.signup_byAuthorizationCode( queryParameters.params.code );
         history.replaceState({}, '', '/dashboard');
       } else { /* check if registration with an access_token is used */
