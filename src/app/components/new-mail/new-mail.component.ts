7a21691ca74b7d310c99ef8626c8de715a5faabf
RMo-Sloth <robmonhemius@gmail.com>
2018-10-18 20:29:56 +0200

    Obtain data from route

--- src/app/components/new-mail/new-mail.component.ts
+++ src/app/components/new-mail/new-mail.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 /* INTERFACES */
-
+import { Character } from '../../interfaces/character';
+import { Mail } from '../../interfaces/mail';
 /* SERVICES */
 import { PageTitleService } from '../../services/page-title.service';
 import { CharacterService } from '../../services/character.service';
@@ -14,7 +15,11 @@ import { CharacterService } from '../../services/character.service';
 })
 export class NewMailComponent implements OnInit {
   public accountIndex: number;
-  public sender;
+  public mailIndex: number;
+  public sender: Character; // Character of accountIndex
+  public mail: Mail; // mail of mailindex
+  public type: string;
+
   public navigationButtons; // TODO: add type
 
   constructor(
@@ -23,6 +28,8 @@ export class NewMailComponent implements OnInit {
     private characterService: CharacterService,
   ) {
     this.accountIndex = parseInt( this.route.snapshot.paramMap.get('account_id') );
+    this.mailIndex = parseInt( this.route.snapshot.paramMap.get('mail_id') );
+    this.type = this.route.snapshot.paramMap.get('type');
     this.navigationButtons = [
       { faClass: 'home', routerUrl: '/dashboard'},
       { faClass: 'envelope', routerUrl: `/${this.accountIndex}/mails`}
@@ -35,6 +42,19 @@ export class NewMailComponent implements OnInit {
       this.sender = character;
       this.pageTitleService.set_pageTitle( `${character.name} - new mail` );
     });
+    // populate mail based on route
+    switch( this.type ) {
+      case 'new':
+      console.log('new');
+      break;
+      case 'reply':
+      console.log('reply');
+        let accountIndex = parseInt( this.route.snapshot.paramMap.get('mail_id') );
+        console.log( accountIndex );
+      break;
+      default:
+      console.log('default fallback (error, should redirect)');
+    }
   }
 
   private sendMail(): void{
