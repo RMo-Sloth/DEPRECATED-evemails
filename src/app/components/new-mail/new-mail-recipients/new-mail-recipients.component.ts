766d3486fa1bccffede9aeb6794ad144f1124b5a
RMo-Sloth <robmonhemius@gmail.com>
2018-10-19 08:58:52 +0200

    @Input/@Output recipients implemented

--- src/app/components/new-mail/new-mail-recipients/new-mail-recipients.component.ts
+++ src/app/components/new-mail/new-mail-recipients/new-mail-recipients.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 /* CHARACTERS */
@@ -22,7 +22,8 @@ export class NewMailRecipientsComponent implements OnInit {
     this.recipients_2$ = new BehaviorSubject( [] );
     this.hideRecipients$ = new BehaviorSubject( false );
   }
-
+  @Input() recipientsIndexes: number[];
+  @Output() recipientsOutput: EventEmitter<number[]> = new EventEmitter();
   public recipients$: BehaviorSubject<Character[]>;
   public recipients_1$: BehaviorSubject<Character[]>;
   public recipients_2$: BehaviorSubject<Character[]>;
@@ -48,6 +49,23 @@ export class NewMailRecipientsComponent implements OnInit {
 
   }
 
+  ngOnChanges() { /* respond to changes on @Input */
+    this.recipientsIndexes.forEach( recipientIndex => {
+      this.recipients$.next( [] );
+      this.characterService.get_character( recipientIndex )
+      .subscribe( character => {
+        let recipients = this.recipients$.getValue();
+        // TODO: check if character already exists in array
+        recipients.push( character );
+        this.recipients$.next( recipients );
+      });
+    });
+    if( this.recipientsIndexes.length === 0 ){
+      this.recipients$.next( [] );
+    }
+    console.log('Input changed');
+  }
+
   private select_recipient(): void {
     // TODO: implement recipient search function
     alert('opening the window to select a recipient has not been implemented yet, but it adds a recipient instead');
@@ -57,17 +75,24 @@ export class NewMailRecipientsComponent implements OnInit {
       // TODO: check if character already exists in array
       recipients.push( character );
       this.recipients$.next( recipients );
+      this.recipientsIndexes.push( character.index );
+      this.recipientsOutput.emit( this.recipientsIndexes );
     });
   }
   private open_recipientDetails( recipient ): void{
     // TODO: implement a recipientDetails screen / component
     alert('clicking temporarily removes the character');
     // TEMP: remove on click
-    let recipients = this.recipients$.getValue();
-    recipients = recipients.filter( currentRecipient => {
-      return currentRecipient.index !== recipient.index;
+    // let recipients = this.recipients$.getValue();
+    // recipients = recipients.filter( currentRecipient => {
+    //   return currentRecipient.index !== recipient.index;
+    // });
+    // this.recipients$.next( recipients );
+    //
+    let recipientsIndexes = this.recipientsIndexes.filter( currentRecipientIndex => {
+      return currentRecipientIndex !== recipient.index;
     });
-    this.recipients$.next( recipients );
+    this.recipientsOutput.emit( recipientsIndexes );
   }
 
 }
--- src/app/components/new-mail/new-mail.component.ts
+++ src/app/components/new-mail/new-mail.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 /* INTERFACES */
 import { Character } from '../../interfaces/character';
 import { Mail } from '../../interfaces/mail';
+
 /* SERVICES */
 import { PageTitleService } from '../../services/page-title.service';
 import { CharacterService } from '../../services/character.service';
+import { MailService } from '../../services/mail.service';
 
 @Component({
   selector: 'app-new-mail',
@@ -18,7 +21,10 @@ export class NewMailComponent implements OnInit {
   public mailIndex: number;
   public sender: Character; // Character of accountIndex
   public mail: Mail; // mail of mailindex
+  public recipients;
   public type: string;
+  public subject$: BehaviorSubject<string>;
+  public mailBody$: BehaviorSubject<string>;
 
   public navigationButtons; // TODO: add type
 
@@ -26,10 +32,14 @@ export class NewMailComponent implements OnInit {
     private route: ActivatedRoute,
     private pageTitleService : PageTitleService,
     private characterService: CharacterService,
+    private mailService: MailService,
   ) {
     this.accountIndex = parseInt( this.route.snapshot.paramMap.get('account_id') );
     this.mailIndex = parseInt( this.route.snapshot.paramMap.get('mail_id') );
     this.type = this.route.snapshot.paramMap.get('type');
+    this.subject$ = new BehaviorSubject( '' );
+    this.mailBody$ = new BehaviorSubject( '' );
+    this.recipients = [];
     this.navigationButtons = [
       { faClass: 'home', routerUrl: '/dashboard'},
       { faClass: 'envelope', routerUrl: `/${this.accountIndex}/mails`}
@@ -45,20 +55,29 @@ export class NewMailComponent implements OnInit {
     // populate mail based on route
     switch( this.type ) {
       case 'new':
-      console.log('new');
-      break;
+        console.log('new');
+        break;
       case 'reply':
-      console.log('reply');
-        let accountIndex = parseInt( this.route.snapshot.paramMap.get('mail_id') );
-        console.log( accountIndex );
-      break;
+        let mailIndex = parseInt( this.route.snapshot.paramMap.get('mail_id') );
+        this.mailService.get_mail( mailIndex, this.accountIndex )
+        .subscribe( mail => {
+          this.subject$.next( `Re: ${mail.subject}` );
+          this.mailBody$.next( mail.body );
+          this.recipients = [ mail.sender ];
+        });
+        break;
       default:
-      console.log('default fallback (error, should redirect)');
+        console.log('default fallback (error, should redirect)');
     }
+  }
+
+  private updateRecipientsArray( recipientIndexes ){
+    this.recipients = recipientIndexes;
+    console.log( recipientIndexes );
   }
 
   private sendMail(): void{
     alert("sending mails isn't implemented yet");
   }
-
 }
+// TODO: should receive array of recipients from the recipientsarray
