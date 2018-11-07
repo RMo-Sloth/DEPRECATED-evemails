96c499f8467a29dd2d4c01456f996edd90157b07
RMo-Sloth <robmonhemius@gmail.com>
2018-11-07 15:20:50 +0100

    Vanilla JS implementation of newsfeed

--- src/app/components/newsfeed/newsfeed.component.ts
+++ src/app/components/newsfeed/newsfeed.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsfeedComponent implements OnInit {
 
-  constructor() { }
+  public newsitem: string;
+  private newsitems: string[];
+
+  constructor() {
+
+    this.newsitem = 'Placeholder s,dngkjfdshgkj hfdkjgh kjsdfgh kj dhfsgk jhdfskj ghkjdfls gkljrs fgkljfkld <a href="http://www.google.com" target="_blank">www.google.com</a> gjldfks';
+  }
 
   ngOnInit() {
+    window.setTimeout( function(){
+
+      
+      // get containers width
+      let newsfeedContainer = document.getElementById('newsfeed-container');
+      let newsfeedContainerWidth = newsfeedContainer.offsetWidth;
+
+      // get newsItem's width
+      let newsItem = document.getElementById('newsitem');
+      let newsItemWidth = newsItem.offsetWidth;
+
+      // set animation speed in px/s
+      let speed = 30;
+
+      // animation duration in ms
+      let duration = ( newsfeedContainerWidth + newsItemWidth ) / speed * 1000;
+
+      let start;
+      function animationStep( timestamp ) {
+        if (!start) start = timestamp;
+        var progress = ( timestamp - start ) / duration;
+        newsItem.style.left =  newsfeedContainerWidth + progress * ( -newsItemWidth + -newsfeedContainerWidth ) + 'px';
+        if (progress < 1) {
+          window.requestAnimationFrame( animationStep );
+        }else{
+          newsItem.style.left = newsfeedContainerWidth + 'px';
+          window.setTimeout( function(){
+            start = null;
+            window.requestAnimationFrame( animationStep );
+          }, 1000);
+        }
+      }
+      start = null;
+      window.requestAnimationFrame( animationStep );
+
+    }, 1000 );
+
+
+
   }
 
 }
